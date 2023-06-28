e9cfe01c5ae2ca7cb48d352515fcef3279336649
Hibatullah Fawwaz Hana <83559752+hib4@users.noreply.github.com>
2023-06-28 11:33:22 +0700

    Time: 169 ms (94.23%), Space: 36.7 MB (95.27%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.kt
+++ 0242-valid-anagram/0242-valid-anagram.kt
@@ -2,16 +2,11 @@ class Solution {
     fun isAnagram(s: String, t: String): Boolean {
         if (s.length != t.length) return false
 
-        val countS = hashMapOf<Char, Int>()
-        val countT = hashMapOf<Char, Int>()
+        val arr = IntArray(26) { 0 }
 
-        for (i in s.indices) {
-            countS[s[i]] = 1 + (countS[s[i]] ?: 0)
-            countT[t[i]] = 1 + (countT[t[i]] ?: 0)
-        }
-        for (j in countS) {
-            if (countS[j.key] != countT[j.key]) return false
-        }
-        return true
+        s.forEach { arr[it - 'a'] += 1 }
+        t.forEach { arr[it - 'a'] -= 1 }
+
+        return arr contentEquals IntArray(26) { 0 }
     }
 }
