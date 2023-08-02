c451e9714dbed3777e74d35c4c121fa17e281ca4
Hibatullah Fawwaz Hana <83559752+hib4@users.noreply.github.com>
2023-08-02 22:10:57 +0700

    Time: 175 ms (99.83%), Space: 36.8 MB (82.06%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.kt
@@ -0,0 +1,23 @@
+class Solution {
+    fun romanToInt(s: String): Int {
+        var ans = 0
+        var num = 0
+        var prev = 0
+
+        for (i in s.length - 1 downTo 0) {
+            when (s[i]) {
+                'I' -> num = 1
+                'V' -> num = 5
+                'X' -> num = 10
+                'L' -> num = 50
+                'C' -> num = 100
+                'D' -> num = 500
+                'M' -> num = 1000
+            }
+            if (num < prev) ans -= num else ans += num
+            prev = num
+        }
+        
+        return ans
+    }
+}
