cb6c7d77716b4d6cde8948cb8ed85cc1ce92c2f9
Hibatullah Fawwaz Hana <83559752+hib4@users.noreply.github.com>
2023-08-02 22:03:31 +0700

    Time: 294 ms (45.41%), Space: 41.7 MB (38.65%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.kt
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.kt
@@ -1,20 +1,17 @@
 class Solution {
     fun topKFrequent(nums: IntArray, k: Int): IntArray {
+        val res = IntArray(k)
+
         val freq = hashMapOf<Int, Int>()
         for (n in nums) {
             freq[n] = freq.getOrDefault(n, 0) + 1
         }
 
-        val bucket = arrayOfNulls<MutableList<Int>>(nums.size + 1)
-        for (i in bucket.indices) bucket[i] = mutableListOf()
-        for (key in freq.keys) bucket[freq[key]!!]?.add(key)
-
-        val res = arrayListOf<Int>()
-        for (i in bucket.size - 1 downTo 0) {
-            res.addAll(bucket[i]!!)
-            if (res.size >= k) break
+        val pair = freq.toList().sortedBy { it.second }
+        for (i in 0 until k) {
+            res[i] = pair[pair.lastIndex - i].first
         }
 
-        return res.toIntArray()
+        return res
     }
 }
