9fa803f777668790820ba091913ef987f4e07bd8
Hibatullah Fawwaz Hana <hiba.kudus@gmail.com>
2023-10-05 11:32:36 +0700

    Time: 1 ms (99.86%), Space: 49.1 MB (5.91%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int m = nums[0];
+        int c = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (c == 0) {
+                c++;
+                m = nums[i];
+            } else if (m == nums[i]) {
+                c++;
+            } else {
+                c--;
+            }
+        }
+        return m;
+    }
+}
