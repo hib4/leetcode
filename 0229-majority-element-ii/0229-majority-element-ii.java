5529ccd2dd0c736b2bc5ea0679a01480015d7e98
Hibatullah Fawwaz Hana <hiba.kudus@gmail.com>
2023-10-05 13:32:02 +0700

    Time: 2 ms (98.52%), Space: 47.1 MB (16.25%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -0,0 +1,39 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        int fm = Integer.MAX_VALUE, fs = 0;
+        int sm = Integer.MIN_VALUE, ss = 0;
+        for (int num : nums) {
+            if (fm == num) {
+                fs++;
+            } else if (sm == num) {
+                ss++;
+            } else if (fs == 0) {
+                fm = num;
+                fs++;
+            } else if (ss == 0) {
+                sm = num;
+                ss++;
+            } else {
+                fs--;
+                ss--;
+            }
+        }
+        fs = 0;
+        ss = 0;
+        for (int num : nums) {
+            if (fm == num) {
+                fs++;
+            } else if (sm == num) {
+                ss++;
+            }
+        }
+        if (fs > nums.length / 3) {
+            res.add(fm);
+        }
+        if (ss > nums.length / 3) {
+            res.add(sm);
+        }
+        return res;
+    }
+}
